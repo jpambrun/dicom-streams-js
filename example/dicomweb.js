4da28945a674cd66e2ab25a15f573a9ed4ee6de7
JF Pambrun <jf.pambrun@gmail.com>
2025-08-20 20:04:35 -0400

    wip

---
+++ example/dicomweb.js
@@ -0,0 +1,79 @@
+import fs from 'node:fs'
+import  { parseFlow, elementFlow, elementSink, pipe, VR, ValueChunk } from 'https://esm.sh/gh/jpambrun/dicom-streams-js@b9f3be0'
+import stream from 'node:stream';
+// https://support.dcmtk.org/docs/dcm2json.html
+
+function tagToString(tag) {
+    const hex = ('00000000' + tag.toString(16)).slice(-8);
+    return hex.toLocaleUpperCase();
+}
+
+class FilterLargeValue extends stream.Transform {
+    constructor(maxSize=100) {
+        super({ objectMode: true });
+        this.maxSize = maxSize;
+    }
+    _transform(chunk, encoding, callback) {
+        if (chunk instanceof ValueChunk) {
+            if (chunk?.bytes?.length > this.maxSize) {
+                chunk.bytes = Buffer.alloc(0);
+            }
+        }
+        callback(null, chunk);
+    }
+}
+
+
+class LogTransform extends stream.Transform {
+    constructor() {
+        super({ objectMode: true });
+    }
+    _transform(chunk, encoding, callback) {
+        console.log(chunk);
+        callback(null, chunk);
+    }
+}
+
+const convertToDicomweb = (src, dst = {}) => {
+    const { characterSets, data } = src;
+    for (const element of data) {
+        const { vr, bigEndian, value, tag, items, fragments } = element;
+        const hextag = tagToString(tag);
+        if (value) {
+            dst[hextag] = {
+                vr: vr.name,
+                value: [value.toString(vr.name, bigEndian, characterSets)],
+            };
+        } else if (items && vr.name === "SQ") {
+            dst[hextag] = {
+                vr: vr.name,
+                value: [],
+            };
+
+            for (const item of items) {
+                const converted = convertToDicomweb(item.elements);
+                dst[hextag].value.push(converted);
+            }
+        } else if (fragments) {
+            //TODO
+            console.log(element);
+        } else {
+            throw new Error(`Unsupported element: ${hextag} with VR ${vr.name}`);
+        }
+    }
+    return dst;
+}
+
+await pipe(
+    fs.createReadStream(process.argv[2]),
+    parseFlow(),
+    new FilterLargeValue(),
+    elementFlow(),
+    // new LogTransform(),
+    elementSink(elements => {
+        console.log(convertToDicomweb(elements));
+    })
+    // new SinkWritable()
+);
+
+
